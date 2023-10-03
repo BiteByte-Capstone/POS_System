f681240016719d957c8ac60a79b6398222fc594a
howard2022 <howard.hyca@gmail.com>
2023-10-02 21:47:36 -0600

    Added code from Thevagi (void button)

---
+++ POS_System/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Models
+{
+    public class OrderedItem
+    {
+        public int ItemId { get; set; } // Add ItemId property
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public double ItemPrice { get; set; }
+    }
+}
+
