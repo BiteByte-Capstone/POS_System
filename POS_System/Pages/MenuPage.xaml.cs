c7146f80d856d5ccfd0c9804f5bd947c10aebd52
howard2022 <howard.hyca@gmail.com>
2023-10-04 13:29:21 -0600

    Updated MenuPage.xaml and .xaml.cs with Ysa one

--- POS_System/Pages/MenuPage.xaml.cs
+++ POS_System/Pages/MenuPage.xaml.cs
@@ -40,6 +40,125 @@ namespace POS_System.Pages
             TypeTextBox.Text = Type;
         }
 
+        private bool CheckForUnpaidOrders(string tableNumber)
+        {
+            // Implement the logic to check for unpaid orders for the specified table number.
+            // You can use a MySQL query for this purpose.
+            string connStr = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // Use a SQL query to check for unpaid orders for the specified table number
+                    string checkOrdersSql = "SELECT COUNT(*) FROM `order` WHERE table_num = @tableNum AND paid = 'n';";
+                    MySqlCommand checkOrdersCmd = new MySqlCommand(checkOrdersSql, conn);
+                    checkOrdersCmd.Parameters.AddWithValue("@tableNum", tableNumber);
+
+                    // Execute the query and get the count of unpaid orders
+                    int unpaidOrderCount = Convert.ToInt32(checkOrdersCmd.ExecuteScalar());
+
+                    // If there are unpaid orders (count > 0), return true; otherwise, return false
+                    return unpaidOrderCount > 0;
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions that may occur during the database operation
+                    MessageBox.Show("Error checking for unpaid orders: " + ex.ToString());
+                    return false; // Return false in case of an error
+                }
+            }
+        }
+
+
+        public MenuPage(string tableNumber, string Type, bool hasUnpaidOrders) : this()
+        {
+            TableNumberTextBox.Text = tableNumber;
+            TypeTextBox.Text = Type;
+
+            if (hasUnpaidOrders)
+            {
+                // Call the method to load unpaid orders for this table.
+                LoadUnpaidOrders(tableNumber);
+            }
+        }
+
+
+        private void LoadUnpaidOrders(string tableNumber)
+        {
+            // Implement the logic to load unpaid orders for the specified table number.
+            // You can use a MySQL query to retrieve the unpaid orders and display them in your UI.
+            string connStr = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // Use a SQL query to retrieve unpaid orders for the specified table number
+                    string unpaidOrdersSql = "SELECT * FROM `order` WHERE table_num = @tableNum AND paid = 'n';";
+                    MySqlCommand unpaidOrdersCmd = new MySqlCommand(unpaidOrdersSql, conn);
+                    unpaidOrdersCmd.Parameters.AddWithValue("@tableNum", tableNumber);
+
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(unpaidOrdersCmd);
+                    DataTable unpaidOrdersTable = new DataTable();
+                    dataAdapter.Fill(unpaidOrdersTable);
+
+                    // Display the unpaid orders in your UI, e.g., in a ListBox or DataGrid
+                    // You can bind the DataTable to your UI element's ItemSource property
+
+                    // Example for binding to a ListBox
+                    OrdersListBox.ItemsSource = unpaidOrdersTable.DefaultView;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading unpaid orders: " + ex.ToString());
+                }
+            }
+        }
+
+
+        private void Open_Table(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                string tableName = button.Name; // get the name of the button
+                string orderType = button.Name; // get the name of the button
+
+                int index = tableName.IndexOf('_'); // get the index number after "_"
+
+                // Show table number or take-out order number
+                string tableNumber = tableName.Substring(index + 1); // remove the first 5 characters ("table")
+                orderType = tableName.Substring(0, index);
+
+                String Type = "";
+                if (orderType.Equals("table"))
+                {
+                    Type = "Dine-In";
+                }
+                else if (orderType.Equals("takeOut"))
+                {
+                    Type = "Take-Out";
+                }
+
+                // Check if there are unpaid orders for the table
+                bool hasUnpaidOrders = CheckForUnpaidOrders(tableNumber);
+
+                // Open the MenuPage with the table number and Type
+                MenuPage menuPage = new MenuPage(tableNumber, Type, hasUnpaidOrders);
+                menuPage.Show();
+                this.Close();
+            }
+        }
+
+        
+
+
+
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -104,7 +223,7 @@ namespace POS_System.Pages
         }
 
 
-        //!!no use code. keep it for refernece 
+
         private void LoadItemsData()
         {
             
@@ -318,7 +437,6 @@ namespace POS_System.Pages
 
         }
 
-        //Void button click event
         private void VoidButton_Click(object sender, RoutedEventArgs e)
         {
             if (OrdersListBox.SelectedItem is Item selectedItem)
@@ -337,6 +455,79 @@ namespace POS_System.Pages
             else
             {
                 MessageBox.Show("Please select an item to void.");
+            }
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a connection string
+            string connStr = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // Insert the order information into the database
+                    string orderSql = "INSERT INTO `order` (table_num, order_timestamp, total_amount, paid) VALUES (@tableNum, @orderTimestamp, @totalAmount, 'n');";
+                    MySqlCommand orderCmd = new MySqlCommand(orderSql, conn);
+                    orderCmd.Parameters.AddWithValue("@tableNum", TableNumberTextBox.Text);
+                    orderCmd.Parameters.AddWithValue("@orderTimestamp", DateTime.Now); // You can adjust this based on your requirement
+                    orderCmd.Parameters.AddWithValue("@totalAmount", TotalAmount); // Assuming TotalAmount is of type double
+                    orderCmd.ExecuteNonQuery();
+
+                    // Get the last inserted order_id
+                    long orderId = orderCmd.LastInsertedId;
+
+                    // Insert the ordered items into the database
+                    foreach (Item orderedItem in Items)
+                    {
+                        // Ensure that the item exists in the `item` table
+                        string checkItemSql = "SELECT item_id FROM item WHERE item_name = @itemName;";
+                        MySqlCommand checkItemCmd = new MySqlCommand(checkItemSql, conn);
+                        checkItemCmd.Parameters.AddWithValue("@itemName", orderedItem.Name);
+                        object itemId = checkItemCmd.ExecuteScalar();
+
+                        if (itemId != null) // Item exists
+                        {
+                            // Insert the ordered item into the `ordered_itemlist` table
+                            string itemSql = "INSERT INTO ordered_itemlist (order_id, item_id, quantity, item_price) VALUES (@orderId, @itemId, @quantity, @itemPrice);";
+                            MySqlCommand itemCmd = new MySqlCommand(itemSql, conn);
+                            itemCmd.Parameters.AddWithValue("@orderId", orderId);
+                            itemCmd.Parameters.AddWithValue("@itemId", itemId);
+                            itemCmd.Parameters.AddWithValue("@quantity", 1); // You can modify this if you track item quantity
+                            itemCmd.Parameters.AddWithValue("@itemPrice", orderedItem.Price);
+                            itemCmd.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Item '{orderedItem.Name}' does not exist in the database.");
+                        }
+                    }
+
+                    MessageBox.Show("Order sent successfully!");
+
+                    // Reset the items list and total amount
+                    Items.Clear();
+                    TotalAmount = 0.0;
+                    TotalAmountTextBlock.Text = TotalAmount.ToString("C");
+
+                    // Open the TablePage
+                    TablePage tablePage = new TablePage();
+                    tablePage.Show();
+                    this.Close();
+                }
+                catch (MySqlException ex)
+                {
+                    // Handle MySQL-specific exceptions
+                    MessageBox.Show("MySQL Error: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    // Handle other exceptions
+                    MessageBox.Show("Error sending order: " + ex.ToString());
+                }
             }
         }
     }
