2cce6acab8363d85254ddbb8856a9cc1bc07dfcd
Piriya-K <piriya619@gmail.com>
2023-10-20 19:59:44 -0600

    Added code from Howard for classes in the Model and Pages packages

---
+++ POS_System/Model/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_System.Models
+{
+    public class Category : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Category() { }
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
