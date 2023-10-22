05994c6cea874c8b895109675fcafdcf5327386d
ysbllcby <ysabellacubay@gmail.com>
2023-10-22 14:47:36 -0600

    PaymentPage working.

--- POS_System/Pages/PaymentPage.xaml.cs
+++ POS_System/Pages/PaymentPage.xaml.cs
@@ -1,44 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using POS_System.Models;
 
 namespace POS_System.Pages
 {
-    /// <summary>
-    /// Interaction logic for PaymentPage.xaml
-    /// </summary>
     public partial class PaymentPage : Window
     {
-
-
         private ObservableCollection<OrderedItem> _orderedItems;
         private string _tableNumber;
         private string _orderType;
         private long _orderId;
         private string _status;
-        private bool _hasUnpaidOrders=true;
+        private bool _hasUnpaidOrders = true;
 
         public PaymentPage()
         {
             InitializeComponent();
+            InitializeEventHandlers();
 
         }
-
-
 
         public PaymentPage(ObservableCollection<OrderedItem> orderedItems, string tableNumber, string orderType, long orderId, string status, bool hasUnpaidOrders) : this()
         {
@@ -51,9 +36,12 @@ namespace POS_System.Pages
 
             CultureInfo cultureInfo = new CultureInfo("en-CA");
             cultureInfo.NumberFormat.CurrencyDecimalDigits = 2;
-            toalAmtTextBox.Text = CalculateTotalOrderAmount().ToString("C", cultureInfo);
-            
-            
+            totalAmtTextBox.Text = CalculateTotalOrderAmount().ToString("C", cultureInfo);
+
+            CalculateTaxAmount();
+            InitializeEventHandlers();
+
+
         }
 
         private double CalculateTotalOrderAmount()
@@ -66,31 +54,6 @@ namespace POS_System.Pages
             return totalAmount;
         }
 
-        private double CalculateTip(double totalOrderAmount)
-        {
-            double tip = 0;
-            double customerPaidAmount = double.Parse(customerPayTextBox.Text);
-            if (decimal.TryParse(customerPayTextBox.Text, out decimal amount))
-            {
-                // Successfully parsed the amount.
-                // Now you can use the 'amount' variable.
-                if (CalculateTotalOrderAmount() > 0)
-                {
-                   tip = customerPaidAmount - CalculateTotalOrderAmount();
-                }
-            }
-            else
-            {
-                // The input was not a valid decimal number.
-                MessageBox.Show("Please enter a valid amount.");
-            }
-
- 
-            return tip;
-        }
-
-
-
 
         private void CancelButton(object sender, RoutedEventArgs e)
         {
@@ -99,10 +62,47 @@ namespace POS_System.Pages
             this.Close();
         }
 
-        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        
+
+        private void CalculateTaxAmount()
         {
-            
-            tipsTextbox.Text = CalculateTip(CalculateTotalOrderAmount()).ToString();
+            double totalTaxAmount = 0;
+            double totalOrderAmount = CalculateTotalOrderAmount();
+            double taxRate = 0.13;
+            totalTaxAmount = totalOrderAmount * taxRate;
+
+            CultureInfo cultureInfo = new CultureInfo("en-CA");
+            cultureInfo.NumberFormat.CurrencyDecimalDigits = 2;
+            totalTaxTextBox.Text = totalTaxAmount.ToString("C", cultureInfo);
         }
+
+        // Calculate Order Total Balance and show in the textbox
+        private void CalculateOrderTotalBalance()
+        {
+            double totalBalance = 0;
+            double totalOrderAmount = CalculateTotalOrderAmount();
+            double totalTaxAmount = Convert.ToDouble(totalTaxTextBox.Text.Substring(1));
+            if (double.TryParse(tipsTextbox.Text, out double tipAmount))
+            {
+                totalBalance = totalOrderAmount + totalTaxAmount + tipAmount;
+
+                CultureInfo cultureInfo = new CultureInfo("en-CA");
+                cultureInfo.NumberFormat.CurrencyDecimalDigits = 2;
+                balanceTextBox.Text = totalBalance.ToString("C", cultureInfo);
+            }
+        }
+
+        private void InitializeEventHandlers()
+        {
+            // Attach the event handler for the TextChanged event of the tipsTextbox
+            tipsTextbox.TextChanged += TipsTextbox_TextChanged;
+        }
+
+        // Handle the TextChanged event of the tipsTextbox to update the balance
+        private void TipsTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CalculateOrderTotalBalance();
+        }
+
     }
 }
