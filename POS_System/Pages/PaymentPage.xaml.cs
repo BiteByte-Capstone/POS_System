bf45dcc413b8aad2f904510dbe3f26f48392d3e6
howard2022 <howard.hyca@gmail.com>
2023-10-23 12:26:53 -0600

    merge PK payment page 90%

--- POS_System/Pages/PaymentPage.xaml.cs
+++ POS_System/Pages/PaymentPage.xaml.cs
@@ -19,6 +19,8 @@ namespace POS_System.Pages
         private string _status;
         private bool _hasUnpaidOrders = true;
 
+        String paymentMethod;
+
         public PaymentPage()
         {
             InitializeComponent();
