c5af4fb777c0b1dc59c713a5ddca0f394c72fbfd
ThevagiVimal <vimthev@gmail.com>
2023-10-27 13:44:07 -0600

    Add category is partially working. edit category function is not working

---
+++ POS_System/EditCategoryDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POS_System
+{
+    /// <summary>
+    /// Interaction logic for EditCategoryDialog.xaml
+    /// </summary>
+    public partial class EditCategoryDialog : Window
+    {
+        public string EditedCategoryName { get; private set; }
+
+        public EditCategoryDialog(string currentCategoryName)
+        {
+            InitializeComponent();
+            CategoryNameTextBox.Text = currentCategoryName;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("SaveButton_Click called");
+                EditedCategoryName = CategoryNameTextBox.Text;
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
--- POS_System/Pages/ManageMenu.xaml.cs
+++ POS_System/Pages/ManageMenu.xaml.cs
@@ -5,11 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
+using POS_System; // Replace with the actual namespace
+
 
 namespace POS_System.Pages
 {
@@ -21,10 +25,16 @@ namespace POS_System.Pages
         private ObservableCollection<Item> items = new ObservableCollection<Item>();
 
         string connectionString = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
+        //Thevagi
+        public bool IsAddCategoryButtonVisible { get; set; }
 
         public ManageMenu()
         {
             InitializeComponent();
+
+            // Set the "Add Category" button's visibility to Collapsed initially THEVAGI
+            DataContext = this; // Set the DataContext to this window
+            IsAddCategoryButtonVisible = false; // Initially hide the button
             LoadCategoryData();
             InitializeMenuItems(); // Load menu items (you should implement this method)
         }
@@ -43,6 +53,9 @@ namespace POS_System.Pages
         private void ShowCategory_Click(object sender, RoutedEventArgs e)
         {
             DataGrid.ItemsSource = GetAllCategory().DefaultView;
+            //Thevagi -
+            // Make the "Add Category" button visible
+            IsAddCategoryButtonVisible = true;
         }
 
         private void ViewSalesButton_Click(object sender, RoutedEventArgs e)
@@ -116,10 +129,9 @@ namespace POS_System.Pages
 
             return dt;
         }
-
+        //new Thevagi
         private void LoadCategoryData()
         {
-
             MySqlConnection conn = new MySqlConnection(connectionString);
 
             try
@@ -145,13 +157,22 @@ namespace POS_System.Pages
                     newCategoryButton.Click += (sender, e) => LoadItemsByCategory(newCategoryButton.Content.ToString());
                     newCategoryButton.Width = 150;
                     newCategoryButton.Height = 60;
-                    newCategoryButton.Margin = new Thickness(5);
                     SetButtonStyle(newCategoryButton);
 
                     CategoryButtonPanel.Children.Add(newCategoryButton);
                 }
 
                 rdr.Close();
+               //  Make the "Add Category" button visible
+                AddCategoryButton.Visibility = Visibility.Visible;
+                // Add "Add Category" button
+                Button addCategoryButton = new Button();
+              addCategoryButton.Content = "Add Category";
+                addCategoryButton.Width = 150;
+                addCategoryButton.Height = 60;
+               addCategoryButton.Click += AddCategoryButton_Click; // Add a handler for this button
+                SetButtonStyle(addCategoryButton);
+               CategoryButtonPanel.Children.Add(addCategoryButton);
             }
             catch (Exception ex)
             {
@@ -160,7 +181,88 @@ namespace POS_System.Pages
             conn.Close();
         }
 
+        // Add Category button click event handler
+        private void AddCategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open the dialog to input the new category name
+            var addCategoryDialog = new AddCategoryDialog();
+            if (addCategoryDialog.ShowDialog() == true)
+            {
+                // Retrieve the category name from the dialog
+                string categoryName = addCategoryDialog.CategoryName;
 
+                if (!string.IsNullOrWhiteSpace(categoryName))
+                {
+                    // Insert the new category into your database
+                    if (InsertCategoryIntoDatabase(categoryName))
+                    {
+                       /* // Reload the category data
+                        LoadCategoryData();*/
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Category name cannot be empty.");
+                }
+            }
+        }
+
+
+        private bool InsertCategoryIntoDatabase(string categoryName)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Insert the new category into your database here
+                    string insertQuery = "INSERT INTO category (category_name) VALUES (@categoryName)";
+                    MySqlCommand cmd = new MySqlCommand(insertQuery, connection);
+                    cmd.Parameters.AddWithValue("@categoryName", categoryName);
+
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while adding the category: " + ex.Message);
+                return false;
+            }
+        }
+
+        private void EditCategory_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the selected category from the DataGrid
+            if (DataGrid.SelectedItem is DataRowView rowView)
+            {
+                // Access the category name from the selected row
+                string categoryName = rowView["category_name"].ToString();
+
+                // Open the EditCategoryDialog to edit the category name
+                var editCategoryDialog = new EditCategoryDialog(categoryName);
+                if (editCategoryDialog.ShowDialog() == true)
+                {
+                    string editedCategoryName = editCategoryDialog.EditedCategoryName;
+                    // Handle the edited category name here
+                }
+                else
+                {
+                    Console.WriteLine("EditCategoryDialog was canceled.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No item selected in DataGrid.");
+            }
+        }
+
+
+
+
+
+        //end Thevagi
 
         private void LoadItemsByCategory(string categoryName)
         {
@@ -204,6 +306,29 @@ namespace POS_System.Pages
                 MessageBox.Show(ex.ToString());
             }
             conn.Close();
+        }
+
+        
+
+
+
+        //Thevagi
+        // Define BoolToVisibilityConverter in code-behind
+        public class BoolToVisibilityConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is bool && (bool)value)
+                {
+                    return Visibility.Visible;
+                }
+                return Visibility.Collapsed;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return value is Visibility && (Visibility)value == Visibility.Visible;
+            }
         }
 
         // For Styling
