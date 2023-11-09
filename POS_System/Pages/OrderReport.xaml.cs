bbbc97e1c74767ec229ebf52e4a2a2f77b1368ce
howard2022 <howard.hyca@gmail.com>
2023-11-09 10:36:06 -0700

    Checkout PK new file

--- POS_System/Pages/OrderReport.xaml.cs
+++ POS_System/Pages/OrderReport.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using POS_System.Models;
+using POS_System.Model;
 
 namespace POS_System.Pages
 {
@@ -26,7 +26,7 @@ namespace POS_System.Pages
         {
             InitializeComponent();
             getDataOrderTable();
-
+    
         }
 
         private void getDataOrderTable()
@@ -68,14 +68,14 @@ namespace POS_System.Pages
         private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             //Tutorial used https://www.youtube.com/watch?v=qZwT_NWQ6Mk&t=14s   
-
+            
             var row = sender as DataGridRow;
             var order = row.DataContext as Order;
 
-            MessageBox.Show(order.OrderId.ToString());
+            MessageBox.Show(order.orderID.ToString());
         }
 
-        private void filterBtn_Click(object sender, RoutedEventArgs e)
+        private void filterBtn_Click(object sender, RoutedEventArgs e) 
         {
             String fromD = fromDate.ToString();
             String untilD = untilDate.ToString();
@@ -95,102 +95,99 @@ namespace POS_System.Pages
 
             String sqlquery = "select * from pos_db.order where ";
 
-            if (fromD.Length + untilD.Length + specificTable.Length + fromTable.Length + toTable.Length + fromAmount.Length + toAmount.Length < 1)
+            if(fromD.Length + untilD.Length + specificTable.Length + fromTable.Length + toTable.Length + fromAmount.Length + toAmount.Length < 1)
             {
-                return;
+                getDataOrderTable();
+            } else {
+
+                //This part checks for the filter that was entered by counting the length of the filter inputs.
+                //If length of filter input > 0, then that filter was used, which then triggers the formulation of a sql query for that filter
+                if (specificTable.Length>0)
+                {
+                    specificTableQuery = SpecificTableFilter(specificTable);
+                    lengthcount[0] = 1;
+                    lengthcount[1] = 0;
+
+                } else if (specificTable.Length < 1 && (fromTable.Length > 0 || toTable.Length > 0))
+                {
+                    fromToTableQuery = FromToTableFilter(fromTable, toTable);
+                    lengthcount[0] = 0;
+                    lengthcount[1] = 1;
+                }
+
+                if (fromAmount.Length > 0 || toAmount.Length > 0)
+                { 
+                    fromToAmountQuery = FromToAmountFilter(fromAmount, toAmount);
+                    lengthcount[2] = 1;
+                }
+
+                if (fromD.Length > 0 || untilD.Length > 0)
+                {
+                    fromToDateQuery = FromToDateFilter(fromD, untilD);
+                    lengthcount[3] = 1;
+                }
+
+                //This part of the code will build the sqlquery to be executed, by adding segments of sqlquery from the filter that was used
+                if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 1 && lengthcount[3] == 1)
+                {
+                    sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery + " and " + fromToAmountQuery + " and " + fromToDateQuery;
+                }
+                else if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 1 && lengthcount[3] == 0)
+                {
+                    sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery + " and " + fromToAmountQuery;
+                }
+                else if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 0 && lengthcount[3] == 1)
+                {
+                    sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery + " and " + fromToDateQuery;
+                } 
+                else if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 0 && lengthcount[3] == 0)
+                {
+                    sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery;
+                } 
+                else if ((lengthcount[0] == 0 || lengthcount[1] == 0) && lengthcount[2] == 1 && lengthcount[3] == 0)
+                {
+                    sqlquery = sqlquery + fromToAmountQuery;
+                }
+                else if ((lengthcount[0] == 0 || lengthcount[1] == 0) && lengthcount[2] == 0 && lengthcount[3] == 1)
+                {
+                    sqlquery = sqlquery + fromToDateQuery;
+                }
+                else if ((lengthcount[0] == 0 || lengthcount[1] == 0) && lengthcount[2] == 1 && lengthcount[3] == 1)
+                {
+                    sqlquery = sqlquery + fromToAmountQuery + " and " + fromToDateQuery;
+                }
+
+                sqlquery = sqlquery + " order by 3;";
+
+
+                //Show the sql query string to be executed
+                MessageBox.Show(sqlquery);
+
+                //String to make connection to database
+                string connectionString = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
+
+                //Create a connection object
+                MySqlConnection connection = new MySqlConnection(connectionString);
+
+                //SQL query
+                MySqlCommand cmd = new MySqlCommand(sqlquery, connection);
+
+                //Open up connection with the user table
+                connection.Open();
+
+                //create a datatable object to capture the database table
+                DataTable dt = new DataTable();
+
+                //Execute the command and the load the result of reader inside the datatable
+                dt.Load(cmd.ExecuteReader());
+
+                //Close connection to user table
+                connection.Close();
+
+                //Bind data table to the DataGrid on XAML
+                orderGrid.DataContext = dt;
+
             }
-
-            //This part checks for the filter that was entered by counting the length of the filter inputs.
-            //If length of filter input > 0, then that filter was used, which then triggers the formulation of a sql query for that filter
-            if (specificTable.Length > 0)
-            {
-                specificTableQuery = SpecificTableFilter(specificTable);
-                lengthcount[0] = 1;
-                lengthcount[1] = 0;
-
-            }
-            else if (specificTable.Length < 1 && (fromTable.Length > 0 || toTable.Length > 0))
-            {
-                fromToTableQuery = FromToTableFilter(fromTable, toTable);
-                lengthcount[0] = 0;
-                lengthcount[1] = 1;
-            }
-
-            if (fromAmount.Length > 0 || toAmount.Length > 0)
-            {
-                fromToAmountQuery = FromToAmountFilter(fromAmount, toAmount);
-                lengthcount[2] = 1;
-            }
-
-            if (fromD.Length > 0 || untilD.Length > 0)
-            {
-                fromToDateQuery = FromToDateFilter(fromD, untilD);
-                lengthcount[3] = 1;
-            }
-
-            //This part of the code will build the sqlquery to be executed, by adding segments of sqlquery from the filter that was used
-            if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 1 && lengthcount[3] == 1)
-            {
-                sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery + " and " + fromToAmountQuery + " and " + fromToDateQuery;
-            }
-            else if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 1 && lengthcount[3] == 0)
-            {
-                sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery + " and " + fromToAmountQuery;
-            }
-            else if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 0 && lengthcount[3] == 1)
-            {
-                sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery + " and " + fromToDateQuery;
-            }
-            else if ((lengthcount[0] == 1 || lengthcount[1] == 1) && lengthcount[2] == 0 && lengthcount[3] == 0)
-            {
-                sqlquery = sqlquery + specificTableQuery + " " + fromToTableQuery;
-            }
-            else if ((lengthcount[0] == 0 || lengthcount[1] == 0) && lengthcount[2] == 1 && lengthcount[3] == 0)
-            {
-                sqlquery = sqlquery + fromToAmountQuery;
-            }
-            else if ((lengthcount[0] == 0 || lengthcount[1] == 0) && lengthcount[2] == 0 && lengthcount[3] == 1)
-            {
-                sqlquery = sqlquery + fromToDateQuery;
-            }
-            else if ((lengthcount[0] == 0 || lengthcount[1] == 0) && lengthcount[2] == 1 && lengthcount[3] == 1)
-            {
-                sqlquery = sqlquery + fromToAmountQuery + " and " + fromToDateQuery;
-            }
-
-            sqlquery = sqlquery + " order by 3;";
-
-
-            //Show the sql query string to be executed
-            MessageBox.Show(sqlquery);
-
-            //String to make connection to database
-            string connectionString = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
-
-            //Create a connection object
-            MySqlConnection connection = new MySqlConnection(connectionString);
-
-            //SQL query
-            MySqlCommand cmd = new MySqlCommand(sqlquery, connection);
-
-            //Open up connection with the user table
-            connection.Open();
-
-            //create a datatable object to capture the database table
-            DataTable dt = new DataTable();
-
-            //Execute the command and the load the result of reader inside the datatable
-            dt.Load(cmd.ExecuteReader());
-
-            //Close connection to user table
-            connection.Close();
-
-            //Bind data table to the DataGrid on XAML
-            orderGrid.DataContext = dt;
-
-
-
-
         }
 
         private String FromToDateFilter(String fromD, String untilD)
@@ -222,22 +219,17 @@ namespace POS_System.Pages
             }
 
             //sql query formulation based on selected filter
-
+            
             //when only the until-date is selected
-            if (fromFormatted.Length > 0 && toFormatted.Length < 1)
-            {
+            if (fromFormatted.Length > 0 && toFormatted.Length < 1) {
                 fromToDateFilterString = " order_timestamp >= '" + fromFormatted + "'";
-
-                //when only the from-date is selected
-            }
-            else if (toFormatted.Length > 0 && fromFormatted.Length < 1)
-            {
+            
+            //when only the from-date is selected
+            } else if (toFormatted.Length > 0 && fromFormatted.Length < 1) {
                 fromToDateFilterString = " order_timestamp <= '" + toFormatted + "' + interval 1 day";
-
-                //when a date-range is selected
-            }
-            else
-            {
+            
+            //when a date-range is selected
+            } else {
                 fromToDateFilterString = " order_timestamp between '" + fromFormatted + "' and '" + toFormatted + "' + interval 1 day";
             }
 
@@ -247,20 +239,18 @@ namespace POS_System.Pages
         private String SpecificTableFilter(String specificTable)
         {
             String specficTableFilterString;
-
+            
             if (specificTable.Contains(","))
             {
                 specficTableFilterString = "table_num in (" + specificTable + ")";
-            }
-            else if (specificTable.Contains(" "))
+            } else if (specificTable.Contains(" ")) 
             {
                 specficTableFilterString = "table_num in (" + specificTable.Replace(" ", ",") + ")";
-            }
-            else
+            } else
             {
                 specficTableFilterString = "table_num = " + specificTable;
             }
-
+                
             return specficTableFilterString;
         }
 
@@ -271,16 +261,14 @@ namespace POS_System.Pages
             if (fromTable.Length > 0 && toTable.Length > 0)
             {
                 fromToTableFilterString = "table_num between " + fromTable + " and " + toTable;
-            }
-            else if (fromTable.Length > 0 && toTable.Length < 1)
+            } else if (fromTable.Length > 0 && toTable.Length < 1)
             {
                 fromToTableFilterString = "table_num >= " + fromTable;
-            }
-            else if (fromTable.Length < 1 && toTable.Length > 0)
+            } else if (fromTable.Length < 1 && toTable.Length > 0)
             {
                 fromToTableFilterString = "table_num <= " + toTable;
             }
-
+            
             return fromToTableFilterString;
         }
 
@@ -311,14 +299,14 @@ namespace POS_System.Pages
             try
             {
                 PrintDialog printDialog = new PrintDialog();
-                if (printDialog.ShowDialog() == true)
+                if(printDialog.ShowDialog() == true)
                 {
                     printDialog.PrintVisual(printReport, "Order Report");
                 }
-            }
+            } 
             catch (Exception ex)
-            {
-
+            { 
+            
             }
         }
 
@@ -326,5 +314,5 @@ namespace POS_System.Pages
 
 
     }
-
+    
 }
