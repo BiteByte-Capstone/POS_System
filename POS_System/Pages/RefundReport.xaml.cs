15d14937e929134486edb93e39ef3aca5590670e
Gurleen Kaur <Gurleen.Kaur02@edu.sait.ca>
2023-11-20 12:24:29 -0700

    Modifying

--- POS_System/Pages/RefundReport.xaml.cs
+++ POS_System/Pages/RefundReport.xaml.cs
@@ -1,81 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using POS_System.Models;
 
 namespace POS_System.Pages
 {
     public partial class RefundReport : Window
     {
-        private List<RefundItem> refundItems; // Assuming RefundItem is your data model class
+        private string connectionString = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
 
         public RefundReport()
         {
             InitializeComponent();
-            refundItems = GetDataRefundTable();
-            UpdateDataGrid(refundItems);
+            GetDataRefundTable();
         }
 
-        private List<RefundItem> GetDataRefundTable()
+        private void GetDataRefundTable()
         {
-            // Retrieve refund data from your data source, you can replace this with your data retrieval logic
-            // For demonstration purposes, I'll create a sample list of RefundItem objects
-            List<RefundItem> refundItems = new List<RefundItem>
+            try
             {
-                new RefundItem { OrderId = 1, ItemId = 101, Name = "Item 1", Quantity = 2, Price = 10.0, Category = "Category A", Date = DateTime.Now },
-                new RefundItem { OrderId = 2, ItemId = 102, Name = "Item 2", Quantity = 3, Price = 15.0, Category = "Category B", Date = DateTime.Now },
-                // Add more refund items here
-            };
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM refund", connection);
 
-            return refundItems;
-        }
+                connection.Open();
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                connection.Close();
 
-        private void UpdateDataGrid(List<RefundItem> filteredItems)
-        {
-            // Bind the filtered data to the DataGrid
-            RefundReportGrid.ItemsSource = filteredItems;
-        }
-
-        private void FilterData()
-        {
-            // Apply filters using LINQ based on the filter criteria
-
-            var filteredItems = refundItems;
-
-            if (!string.IsNullOrWhiteSpace(orderIdBoxFilter.Text))
-            {
-                int orderId = int.Parse(orderIdBoxFilter.Text);
-                filteredItems = filteredItems.Where(item => item.OrderId == orderId).ToList();
+                refundGrid.DataContext = dt;
             }
-
-            if (!string.IsNullOrWhiteSpace(itemIdBoxFilter.Text))
+            catch (Exception ex)
             {
-                int itemId = int.Parse(itemIdBoxFilter.Text);
-                filteredItems = filteredItems.Where(item => item.ItemId == itemId).ToList();
+                MessageBox.Show("Error: " + ex.Message);
             }
-
-            if (!string.IsNullOrWhiteSpace(categoryBoxFilter.Text))
-            {
-                string category = categoryBoxFilter.Text;
-                filteredItems = filteredItems.Where(item => item.Category.Equals(category, StringComparison.OrdinalIgnoreCase)).ToList();
-            }
-
-            // Add more filter criteria as needed
-
-            UpdateDataGrid(filteredItems);
         }
 
-        private void filterBtn_Click(object sender, RoutedEventArgs e)
+        private void FilterBtn_Click(object sender, RoutedEventArgs e)
         {
-            FilterData();
+            try
+            {
+                string fromTimestamp = refundTimestampFilter.SelectedDate?.ToString("yyyy-MM-dd");
+                string untilTimestamp = refundTimestampFilter.SelectedDate?.ToString("yyyy-MM-dd");
+                string specificMethod = refundMethodFilter.Text;
+
+                string specificUser = userIdFilter.Text;
+
+                string sqlQuery = "SELECT * FROM refund WHERE 1=1";
+
+                if (!string.IsNullOrEmpty(fromTimestamp) && !string.IsNullOrEmpty(untilTimestamp))
+                {
+                    sqlQuery += " AND refund_timestamp BETWEEN @fromTimestamp AND @untilTimestamp";
+                }
+                else if (!string.IsNullOrEmpty(fromTimestamp))
+                {
+                    sqlQuery += " AND refund_timestamp >= @fromTimestamp";
+                }
+                else if (!string.IsNullOrEmpty(untilTimestamp))
+                {
+                    sqlQuery += " AND refund_timestamp <= @untilTimestamp";
+                }
+
+                if (!string.IsNullOrEmpty(specificMethod))
+                {
+                    sqlQuery += " AND refund_method = @specificMethod";
+                }
+
+                if (!string.IsNullOrEmpty(specificUser))
+                {
+                    sqlQuery += " AND user_id = @specificUser";
+                }
+
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                MySqlCommand cmd = new MySqlCommand(sqlQuery, connection);
+
+                // Add parameters for user inputs
+                cmd.Parameters.AddWithValue("@fromTimestamp", fromTimestamp);
+                cmd.Parameters.AddWithValue("@untilTimestamp", untilTimestamp);
+                cmd.Parameters.AddWithValue("@specificMethod", specificMethod);
+                cmd.Parameters.AddWithValue("@specificUser", specificUser);
+
+                connection.Open();
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                connection.Close();
+
+                refundGrid.DataContext = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
 
-        private void printBtn_Click(object sender, RoutedEventArgs e)
+        private void PrintBtn_Click(object sender, RoutedEventArgs e)
         {
-            // Add your print logic here, you can use a reporting library or framework to generate and print the refund report
-            MessageBox.Show("Printing the refund report...");
+            try
+            {
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog() == true)
+                {
+                    printDialog.PrintVisual(refundGrid, "Refund Report");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
