6aa592c76e541bb9a94c840d24c0570391deca34
Piriya-K <piriya619@gmail.com>
2023-11-24 22:22:25 -0700

    Revert "AdminPage"
    This reverts commit 6fedcd65b320ca3570b241caeedb03dfe2f1b2e8.

--- POS_System/Pages/AdminPage.xaml.cs
+++ POS_System/Pages/AdminPage.xaml.cs
@@ -27,26 +27,13 @@ namespace POS_System.Pages
     /// </summary>
     public partial class AdminPage : Window
     {
-        List<String> managerList = new List<string> {"Waiter"};
-        List<String> AdminList = new List<string> { "Waiter", "Admin", "Manager" };
-
 
         private DatabaseHelper db;
 
         public AdminPage()
         {
             InitializeComponent();
-            if (POS.Models.User.id >= 200)
-            {
-                GetOnlyWaiter();
-                UserCombobox.ItemsSource = managerList;
-
-            }
-            else
-            {
-                getAllUser();
-                UserCombobox.ItemsSource = AdminList;
-            }
+            getAllUser();
         }
 
         private void getAllUser()
@@ -77,44 +64,6 @@ namespace POS_System.Pages
             connection.Close();
 
             userGrid.DataContext = dt;
-        }
-
-        private void GetOnlyWaiter()
-        {
-            //Tutorial used https://www.youtube.com/watch?v=OPDPI5exPp8
-
-            //db = new DatabaseHelper("localhost", "pos_db", "root", "password");
-
-            //String to make connection to database
-            string connectionString = "SERVER=localhost;DATABASE=pos_db;UID=root;PASSWORD=password;";
-
-            //Create a connection object
-            MySqlConnection connection = new MySqlConnection(connectionString);
-
-            //SQL query
-            MySqlCommand cmd = new MySqlCommand("select * from user where user_id >= 300 order by 1", connection);
-
-            //Open up connection with the user table
-            connection.Open();
-
-            //create a datatable object to capture the database table
-            DataTable dt = new DataTable();
-
-            //Execute the command and the load the result of reader inside the datatable
-            dt.Load(cmd.ExecuteReader());
-
-            //Close connection to user table
-            connection.Close();
-
-            userGrid.DataContext = dt;
-
-
-        }
-
-        private void MakeComboBoxReadOnly()
-        {
-            ComboBox comboBox = new ComboBox();
-            comboBox.IsReadOnly = true;
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -305,10 +254,14 @@ namespace POS_System.Pages
 
         private void RoleComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-           
-                string selectedRole = UserCombobox.SelectedItem.ToString();
+            ComboBox comboBox = sender as ComboBox;
+
+            if (comboBox.SelectedItem is ComboBoxItem item)
+            {
+                string selectedRole = (comboBox.SelectedItem as ComboBoxItem).Content.ToString();
                 int newID = GetNextIDForRole(selectedRole);
                 adduser_idBox.Text = newID.ToString();
+            }
         }
 
         private int GetNextIDForRole(string role)
